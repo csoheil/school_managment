89a5c44f9044f1eaa7334fd6a3a48be0eefba5f7
soheil mohammadi <soheilmohammadi0o19@gmail.com>
2025-09-08 16:09:13 +0330

    Add assignments table migration

--- database/migrations/2025_09_08_123718_create_assignments_table.php
+++ database/migrations/2025_09_08_123718_create_assignments_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,14 @@ return new class extends Migration
     {
         Schema::create('assignments', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->unsignedBigInteger('class_id');
+            $table->unsignedBigInteger('teacher_id');
+            $table->string('file_path');
+            $table->dateTime('due_date');
             $table->timestamps();
+            $table->foreign('class_id')->references('id')->on('classes')->onDelete('cascade');
+            $table->foreign('teacher_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
