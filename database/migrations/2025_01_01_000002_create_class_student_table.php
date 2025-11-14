298a1f9b51e502200680146c49065a27e4d596ef
soheil mohammadi <soheilmohammadi0o19@gmail.com>
2025-11-14 13:43:50 +0330

    feat(db): add pivot table class_student

---
+++ database/migrations/2025_01_01_000002_create_class_student_table.php
@@ -0,0 +1,5 @@
+Schema::create('class_student', function (Blueprint $table) {
+$table->foreignId('class_id')->constrained()->cascadeOnDelete();
+$table->foreignId('student_id')->constrained('users')->cascadeOnDelete();
+$table->primary(['class_id', 'student_id']);
+});
