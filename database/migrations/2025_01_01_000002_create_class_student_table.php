102f88b7614759f1b810b4239dc881a5ae3685ff
soheil mohammadi <soheilmohammadi0o19@gmail.com>
2025-11-14 13:44:38 +0330

    fix: fix a problem that add by mistakes

--- database/migrations/2025_01_01_000002_create_class_student_table.php
+++ database/migrations/2025_01_01_000002_create_class_student_table.php
@@ -1,3 +1,5 @@
+<?php
+
 Schema::create('class_student', function (Blueprint $table) {
 $table->foreignId('class_id')->constrained()->cascadeOnDelete();
 $table->foreignId('student_id')->constrained('users')->cascadeOnDelete();
